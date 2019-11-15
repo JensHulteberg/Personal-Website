812bfe320dc46147e840a1f28fa1036d82f066d8
Jens H <jens.hulteberg@gmail.com>
2019-11-15 10:33:28 +0100

    Top bar is now kinda how i want it

--- src/professionalComponents/ProfessionalTopbar.js
+++ src/professionalComponents/ProfessionalTopbar.js
@@ -2,20 +2,64 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class ProfessionalTopbar extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
     render() {
+
         return (
             <div className="Topbar">
                 <AppBar position="fixed">
-                    <Toolbar className="toolbar">
-                        <Box fontWeight="fontWeightLight" m={1}>
-                            Light
-                            </Box>
+                    <Toolbar className="toolbar" variant="dense">
+                        <Grid
+                            justify="space-between"
+                            container
+                        >
+                            <Grid item>
+                                <Box fontWeight="fontWeightLight">
+                                    <Typography>
+                                        Jens Hulteberg
+                                    </Typography>
+                                </Box>
+                            </Grid>
+                            <Grid item>
+                                <Box fontWeight="fontWeightLight">
+                                    <Grid container
+                                        spacing={1}>
+                                        <Grid item>
+                                            <Typography>
+                                                Profile
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography>
+                                                Contact
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography>
+                                                Blog
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            </Grid>
+                        </Grid>
                     </Toolbar>
                 </AppBar>
-            </div>
+            </div >
         )
     }
 }
